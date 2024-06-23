20a94accc2ff9a7403d62e57cc05ca1d1be5c6f9
Mohamed <arshathm4@gmail.com>
2024-06-23 12:47:01 +0530

    feat: project initial setup

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+
+const app = express();
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
